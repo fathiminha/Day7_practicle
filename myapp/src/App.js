efd3609cb887bb8aa1db279edf01cf5c123e84bf
Fathima Minha <139671210+fathiminha@users.noreply.github.com>
2024-11-22 11:17:32 +0000

    parse message from child-parent

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Childcom from './components/Childcom';
+import {useState} from 'react';
 
 function App() {
+  const [childname,setChildname] = useState("Default");
+  const receiveName=(name)=>{
+    setChildname(name);
+  }
+
   return (
     <div className="App">
       <h2> Hello React I'm Parent </h2>
-      
+      <Childcom setfun={receiveName} />
+      <p> Parent: My child said: {childname} </p>
     </div>
   );
 }
